ce1c9178cbc18cfe06dce53e24dd3420b496c651
Mekal <23715925+MekalBoy@users.noreply.github.com>
2024-12-28 16:00:23 +0200

    Client2Client works
    Clients work properly now.
    Initialization shares hash data from tracker to clients so they know what to look for.
    Clients now write wanted files to disk once complete.

--- src/tema2.cpp
+++ src/tema2.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <climits>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -48,12 +49,11 @@ vector<string> filesWanted;
 
 bool finished = false;
 
-char filename[MAX_FILENAME];
-
 void *download_thread_func(void *arg) {
 	int rank = *(int *)arg;
 
 	int syncTimer = SYNC_TIMER;
+	char filename[MAX_FILENAME];
 
 	// anti-choke algorithm?
 	pmr::unordered_map<int, int> disturbances;
@@ -83,6 +83,8 @@ void *download_thread_func(void *arg) {
 					// cout << endl;
 				}
 			}
+		} else {
+			syncTimer++;
 		}
 
 		// pick a file of the ones I want (randomly)
@@ -90,21 +92,75 @@ void *download_thread_func(void *arg) {
 		// pick a seeder (the one disturbed least, randomly, increment "disturbance")
 		// if I got the segment - increment currSegment of the file, increment syncTimer
 
-		// strncpy(filename, filesWanted[rand() % filesWanted.size()].c_str(), MAX_FILENAME);
+		strncpy(filename, filesWanted[rand() % filesWanted.size()].c_str(), MAX_FILENAME);
 
-		// fileInfo wantedFile = myFiles[filename];
+		fileInfo *wantedFile = &myFiles[filename];
 
-		// hashInfo wantedHash = wantedFile.hashes[wantedFile.segmentsNr];
+		hashInfo *wantedHash = &wantedFile->hashes[wantedFile->segmentsNr];
 
-		// int leastDisturbed = -1;
-		// for (int peer : wantedFile.owners) {
+		vector<int> leastDisturbed;
+		int minDisturbance = INT_MAX;
+		for (int peer : wantedFile->owners) {
+			// never seen this guy before - add to disturbances list (we may disturb 'em later)
+			if (disturbances.find(peer) == disturbances.end()) {
+				disturbances[peer] = 0;
+			}
 
-		// }
+			if (disturbances[peer] < minDisturbance) {
+				minDisturbance = disturbances[peer];
+				leastDisturbed.clear();
+				leastDisturbed.push_back(peer);
+			} else if (disturbances[peer] == minDisturbance) {
+				leastDisturbed.push_back(peer);
+			}
+		}
 
-		// download x10, sync, repeat
-		nrFilesWanted--;
-		filename[0] = '\0';
-		MPI_Send(&filename, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+		// possible victims - now randomly choose one!
+		int chosenPeer = leastDisturbed[rand() % leastDisturbed.size()];
+		printf("Client %d seeking file \"%s\" hash #%d - %s from %d\n", rank, filename, wantedFile->segmentsNr, wantedHash->hash, chosenPeer);
+
+		MPI_Send(filename, MAX_FILENAME, MPI_CHAR, chosenPeer, 1, MPI_COMM_WORLD);
+		MPI_Send(&wantedHash->hash, HASH_SIZE, MPI_CHAR, chosenPeer, 2, MPI_COMM_WORLD);
+
+		MPI_Recv(&wantedHash->owned, 1, MPI_INT, chosenPeer, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		// Successfully received another hash
+		if (wantedHash->owned == 1) {
+			wantedFile->segmentsNr++;
+
+			// succesful disturbance - increment disturbances
+			disturbances[chosenPeer]++;
+
+			// Successfully completed a wanted file
+			if (wantedFile->segmentsNr == wantedFile->completeNr - 1) { // segmentsNr starts at 0
+				nrFilesWanted--;
+				wantedFile->complete = true;
+
+				filesWanted.erase(find(filesWanted.begin(), filesWanted.end(), filename));
+
+				// Notify tracker
+				MPI_Send(&filename, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+
+				// write the now-complete file in clientR_filename
+				ofstream clientFile;
+				string clientFilename = "client" + to_string(rank) + "_" + filename;
+				clientFile.open(clientFilename);
+				printf("Client %d writing file \"%s\" to disk...\n", rank, filename);
+				for (int i = 0; i < wantedFile->completeNr; i++) {
+					string hashString = wantedFile->hashes[i].hash;
+					clientFile << hashString.substr(0, HASH_SIZE);
+					if (i != wantedFile->completeNr - 1) {
+						clientFile << endl;
+					}
+				}
+				clientFile.close();
+			}
+		} else {
+			// maybe add more because of an unsuccessful disturbance - should give the peer more time to gather other hashes
+			disturbances[chosenPeer] += 5;
+			// test through `time make run` for quickness and `make run | wc -l` for miss-count
+			// the lower the better for both
+		}
 	}
 
 	// once all wanted files are downloaded write them in the clientR_filename
@@ -115,25 +171,27 @@ void *download_thread_func(void *arg) {
 void *upload_thread_func(void *arg) {
 	int rank = *(int *)arg;
 
+	char filenameUpload[MAX_FILENAME];
+
 	while (true) {
-		MPI_Recv(&filename, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &mpiStatus);
+		MPI_Recv(&filenameUpload, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &mpiStatus);
 
 		// tracker will never send a "tag 1" except for when everyone has finished
-		if (mpiStatus.MPI_SOURCE == TRACKER_RANK) {
+		if (mpiStatus.MPI_SOURCE == TRACKER_RANK && mpiStatus.MPI_TAG == 1) {
 			break;
 		}
 
-		fileInfo &requestedFile = myFiles[filename];
+		fileInfo &requestedFile = myFiles[filenameUpload];
 
 		char requestedHash[HASH_SIZE];
 
-		MPI_Recv(&requestedHash, HASH_SIZE, MPI_CHAR, mpiStatus.MPI_SOURCE, 1, MPI_COMM_WORLD, &mpiStatus);
+		MPI_Recv(&requestedHash, HASH_SIZE, MPI_CHAR, mpiStatus.MPI_SOURCE, 2, MPI_COMM_WORLD, &mpiStatus);
 
 		// check if we have it and send a yes/no reply (1/0 int)
 
 		for (hashInfo possibleHash : requestedFile.hashes) {
 			if (strncmp(possibleHash.hash, requestedHash, HASH_SIZE) == 0) {
-				MPI_Send(&possibleHash.owned, 1, MPI_INT, mpiStatus.MPI_SOURCE, 1, MPI_COMM_WORLD);
+				MPI_Send(&possibleHash.owned, 1, MPI_INT, mpiStatus.MPI_SOURCE, 3, MPI_COMM_WORLD);
 			}
 		}
 	}
@@ -207,7 +265,7 @@ void tracker(int numtasks, int rank) {
 		cout << endl;
 	}
 
-	// Receive wanted files from clients
+	// Receive wanted files from clients (and send hash information for each)
 
 	int wantedNr = 0; // we use this to know when to close the swarm
 
@@ -223,6 +281,21 @@ void tracker(int numtasks, int rank) {
 		MPI_Recv(&wanted, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 		wantedNr += wanted;
+
+		// also send hash information back for each file
+		for (int j = 0; j < wanted; j++) {
+			char wantedFilename[MAX_FILENAME];
+			MPI_Recv(wantedFilename, MAX_FILENAME, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			// printf("[TRACKER] Client %d wants info for file %s\n", i, wantedFilename);
+
+			fileInfo wantedFile = trackerFiles[wantedFilename];
+
+			MPI_Send(&wantedFile.completeNr, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+			for (hashInfo info : wantedFile.hashes) {
+				MPI_Send(info.hash, HASH_SIZE, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+			}
+		}
 	}
 
 	printf("[TRACKER] There are %d wanted files across the whole cluster.\n", wantedNr);
@@ -234,9 +307,9 @@ void tracker(int numtasks, int rank) {
 
 	// While loop to check for requests
 	while (1) {
-		char filename[FILENAME_MAX];
+		char filename[MAX_FILENAME];
 
-		MPI_Recv(&filename, FILENAME_MAX, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &mpiStatus);
+		MPI_Recv(&filename, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &mpiStatus);
 
 		// Deduce request type by tag
 		switch (mpiStatus.MPI_TAG) {
@@ -372,6 +445,22 @@ void peer(int numtasks, int rank) {
 	}
 
 	MPI_Send(&nrFilesWanted, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+
+	for (int i = 0; i < nrFilesWanted; i++) {
+		const char *wantedFilename = filesWanted[i].c_str();
+		MPI_Send(wantedFilename, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+
+		MPI_Recv(&myFiles[wantedFilename].completeNr, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Client %d knows to look for %d hashes in %s\n", rank, myFiles[wantedFilename].completeNr, wantedFilename);
+		for (int j = 0; j < myFiles[wantedFilename].completeNr; j++) {
+			hashInfo newHash;
+			newHash.owned = false;
+			newHash.id = j;
+			MPI_Recv(newHash.hash, HASH_SIZE, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			myFiles[wantedFilename].hashes.push_back(newHash);
+		}
+	}
 
 	// Await OK signal (no data)
 
